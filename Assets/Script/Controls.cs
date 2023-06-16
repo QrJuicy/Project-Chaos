9c45cbbf156ee8c7f5771a2d9eb28fdd171a715f
bryan-wilberd <bryan.w.l.1005@gmail.com>
2023-06-16 15:11:19 +0700

    added gravity

--- Assets/Script/Controls.cs
+++ Assets/Script/Controls.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
+    private Vector3 movement;
+    [SerializeField] float gravity;
     private CharacterController ctrl;
     [SerializeField] float speed;
     void Start()
@@ -13,8 +15,11 @@ public class Controls : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        ctrl.Move(move * Time.deltaTime * speed);
+        Vector3 move = new Vector3 (Input.GetAxis("Horizontal") , 0 , Input.GetAxis("Vertical")).normalized;
+        movement = move * Time.deltaTime * speed;
+        movement += new Vector3 (0 , gravity * Time.deltaTime , 0);
+        ctrl.Move(movement);              
     }
 
 }
+
