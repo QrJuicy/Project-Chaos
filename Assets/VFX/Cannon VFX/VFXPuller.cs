28f548a7e2b21e5910b7afcda4d142e1129660b5
Darren <derenpx@gmail.com>
2023-06-21 17:32:03 +0700

    Adding VFX to Cannon, fixing minor bugs in sound and textures
    Adding VFX to Cannon+Trigger Script, adding sfx to cannon, changing bgm, fixing flags material, fixing sword material

---
+++ Assets/VFX/Cannon VFX/VFXPuller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFXPuller : MonoBehaviour
+{
+    [SerializeField] VisualEffect _Pull_From_PreFab;
+    [SerializeField] AudioSource _Add_SFX;
+    private float SpawnInterfal = 6f;
+
+    private void Start()
+    {
+        SpawnVFX();
+    }
+
+    void SpawnVFX()
+    {
+        Invoke("SpawnVFX",SpawnInterfal);
+        //instantiate new vfx
+        VisualEffect newSpawnedVFX = Instantiate(_Pull_From_PreFab, transform.position, transform.rotation);
+        //play vfx
+        newSpawnedVFX.Play();
+        _Add_SFX.Play();
+        //destroy vfx
+        Destroy(newSpawnedVFX.gameObject, 5f);
+        
+    }
+}
